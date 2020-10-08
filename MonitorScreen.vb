818b6416c39205c8688ddb0bffb3528cccf5c57c
Jonathan Grunder <jonathan.grunder@comdatagroup.fr>
2020-10-08 16:13:06 +0200

    Add screen id parameter

--- MonitorScreen.vb
+++ MonitorScreen.vb
@@ -1,9 +1,12 @@
 ﻿Imports System.Configuration
 
 Public Class MonitorScreen
+    ' Get all parameters from application configuration file
+    Private ReadOnly Url As String = ConfigurationManager.AppSettings.Get("url")
+    Private ReadOnly ScreenId As Integer = ConfigurationManager.AppSettings.Get("screen_id")
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim uri As Uri = New Uri(ConfigurationManager.AppSettings.Get("url"))
+        Dim uri As Uri = New Uri(Url)
         webView.Source = uri
-        Location = Screen.AllScreens(1).WorkingArea.Location
+        Location = Screen.AllScreens(ScreenId).WorkingArea.Location
     End Sub
 End Class
